59cd60f22d2a78d4acf1346844f0c42d38081bc3
Ashwin Samudre <capnoi8@gmail.com>
2018-12-28 16:24:40 +0530

    Update summary_script.py

--- summary_script.py
+++ summary_script.py
@@ -9,52 +9,63 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient[dbname]
 mycol = mydb[colname]
 
-
-#path_to_GARUDATA = input()
-#TODO : Remove the hardcoded directory path
 #TODO : Create a final dictionary and write to the database
 
-flag = 0
-for directory,subdir,files in os.walk('/home/mahak/NCRA/summary_files/GARUDATA'):
-	for fname in files:
-		fn = directory + "/" + fname
-		fp = open(fn)
-		lines = fp.readlines()
-		document = {}
-		dict = {}
-		for line in lines:
-			flag = 1
-			if 'processing took' in line:
-				if 'day' in line:
-					line = line.split(" ")
-					total_time = (int(line[3]) * 24 * 60)
-					temp_time = line[5].split(":")  
-					total_time += int(temp_time[0]) * 60 + int(temp_time[1])
-					seconds = temp_time[2]
-					print("Total time required for processing is : " + str(total_time) + " minutes " + seconds + " seconds")
-				else:
-					line = line.split(" ")
-					temp_time = line[3].split(":")
-					total_time = int(temp_time[0]) * 60 + int(temp_time[1])
-					seconds = temp_time[2]
-					print("Total time required for processing is : " + str(total_time) + " minutes " + seconds + " seconds") 
-			
-			if 'image' in line:
-				line = line.split(" ")
-				index = line.index("image:") - 1
-				keyname = line[index]
-				visibility = int(line[line.index("visibilities,") - 1])
-				flux = float(line[line.index("Jy") - 1])
-				clean_components = int(line[line.index("CLEAN") - 1])
-				rms = float(line[line.index("mJy/beam") - 1])
-				dict[keyname] = {"visibilities" : visibility , "flux" : flux , "clean_components" : clean_components , "rms" : rms}
-				#print(dict)
-		if flag == 1:
-			fname = fname.split("_")
-			source = fname[1]
-			freq = fname[2][4:]
-			
-			dirlist = directory.split("/")
-			obsno = dirlist[-3]
-			cycleno = dirlist[-4]
-			date = dirlist[-2].split("_")[1]
+def summary(path_to_GARUDATA):
+    """Function to parse the required data - required time, 
+    frequency, visibilities, flux, clean_components, RMS,
+    source, observation number, cycle number, date.
+    """
+
+    flag = 0
+    for root,dirs,files in os.walk(path_to_GARUDATA):
+        for fname in files:
+            fn = root + "/" + fname
+            fp = open(fn)
+            lines = fp.readlines()
+            document = {}
+            dict = {}
+            for line in lines:
+                flag = 1
+                if 'processing took' in line:
+                    if 'day' in line:
+                        line = line.split(" ")
+                        total_time = (int(line[3]) * 24 * 60)
+                        temp_time = line[5].split(":")  
+                        total_time += int(temp_time[0]) * 60 + int(temp_time[1])
+                        seconds = temp_time[2]
+                        print("Total time required for processing is : " + str(total_time) + " minutes " + seconds + " seconds")
+                    else:
+                        line = line.split(" ")
+                        temp_time = line[3].split(":")
+                        total_time = int(temp_time[0]) * 60 + int(temp_time[1])
+                        seconds = temp_time[2]
+                        print("Total time required for processing is : " + str(total_time) + " minutes " + seconds + " seconds") 
+
+                if 'image' in line:
+                    line = line.split(" ")
+                    index = line.index("image:") - 1
+                    keyname = line[index]
+                    visibility = int(line[line.index("visibilities,") - 1])
+                    flux = float(line[line.index("Jy") - 1])
+                    clean_components = int(line[line.index("CLEAN") - 1])
+                    rms = float(line[line.index("mJy/beam") - 1])
+                    dict[keyname] = {"visibilities" : visibility , "flux" : flux , "clean_components" : clean_components , "rms" : rms}
+
+            if flag == 1:
+                fname = fname.split("_")
+                source = fname[1]
+                freq = fname[2][4:]
+            
+                dirlist = root.split("/")
+                obsno = dirlist[-3]
+                cycleno = dirlist[-4]
+                date = dirlist[-2].split("_")[1]
+
+def main():
+	print("Please enter the absolute path to GARUDATA")
+	path_to_GARUDATA = input()
+	summary(path_to_GARUDATA)
+
+if __name__ == "__main__":
+	main()
