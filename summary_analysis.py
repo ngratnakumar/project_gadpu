28b0c145bf7f9104ccf3b95f6aaa6504a307d586
Mahak <mahak.bansal97@gmail.com>
2019-01-02 10:09:05 +0530

    Read database into a dataframe

--- summary_analysis.py
+++ summary_analysis.py
@@ -3,6 +3,12 @@ import sys
 import pprint
 from matplotlib import pyplot as plt
 import seaborn as sns
+import numpy as np
+import pandas as pd 
+from scipy import stats
+from matplotlib.cbook import boxplot_stats
+
+
 
 dbname = "summary_db"
 
@@ -11,6 +17,154 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient[dbname]
 
 pp = pprint.PrettyPrinter()
+
+def read_database(frequency):
+    print("start")
+    collection = mydb['CYCLE15']
+    # cursor = collection.find({})
+
+
+    cursor = collection.find({'frequency' : frequency})
+    # for doc in cursor:
+    #     print(doc['summary'])
+    sources = []
+
+    MC1_visibilities = []
+    MC1_flux = []
+    MC1_clean = []
+    MC1_rms = []
+
+    SC1_visibilities = []
+    SC1_flux = []
+    SC1_clean = []
+    SC1_rms = []
+
+
+    SC2_visibilities = []
+    SC2_flux = []
+    SC2_clean = []
+    SC2_rms = []
+
+    
+    SC3_visibilities = []
+    SC3_flux = []
+    SC3_clean = []
+    SC3_rms = []
+
+    SP1_visibilities = []
+    SP1_flux = []
+    SP1_clean = []
+    SP1_rms = []
+
+    SP1A_visibilities = []
+    SP1A_flux = []
+    SP1A_clean = []
+    SP1A_rms = []
+
+    SP1B_visibilities = []
+    SP1B_flux = []
+    SP1B_clean = []
+    SP1B_rms = []
+
+    SP2_visibilities = []
+    SP2_flux = []
+    SP2_clean = []
+    SP2_rms = []
+
+    SP2A_visibilities = []
+    SP2A_flux = []
+    SP2A_clean = []
+    SP2A_rms = []
+
+    SP2B_visibilities = []
+    SP2B_flux = []
+    SP2B_clean = []
+    SP2B_rms = []
+
+    # data = pd.DataFrame(list(cursor))
+    # pp.pprint(data)
+
+    for document in cursor:  
+        
+        sources.append(document["source"])
+
+        MC1_visibilities.append(document['summary']['MC1']['visibilities'])
+        MC1_flux.append(document['summary']['MC1']['flux'])
+        MC1_clean.append(document['summary']['MC1']['clean_components'])
+        MC1_rms.append(document['summary']['MC1']['rms'])
+
+        SC1_visibilities.append(document['summary']['SC1']['visibilities'])
+        SC1_flux.append(document['summary']['SC1']['flux'])
+        SC1_clean.append(document['summary']['SC1']['clean_components'])
+        SC1_rms.append(document['summary']['SC1']['rms'])
+
+        SC2_visibilities.append(document['summary']['SC2']['visibilities'])
+        SC2_flux.append(document['summary']['SC2']['flux'])
+        SC2_clean.append(document['summary']['SC2']['clean_components'])
+        SC2_rms.append(document['summary']['SC2']['rms'])
+
+        SC3_visibilities.append(document['summary']['SC3']['visibilities'])
+        SC3_flux.append(document['summary']['SC3']['flux'])
+        SC3_clean.append(document['summary']['SC3']['clean_components'])
+        SC3_rms.append(document['summary']['SC3']['rms'])
+
+        try:
+            SP1_visibilities.append(document['summary']['SP1']['visibilities'])
+            SP1_flux.append(document['summary']['SP1']['flux'])
+            SP1_clean.append(document['summary']['SP1']['clean_components'])
+            SP1_rms.append(document['summary']['SP1']['rms'])
+
+        except KeyError:
+            SP1_visibilities.append(document['summary']['SP0']['visibilities'])
+            SP1_flux.append(document['summary']['SP0']['flux'])
+            SP1_clean.append(document['summary']['SP0']['clean_components'])
+            SP1_rms.append(document['summary']['SP0']['rms'])
+
+
+        SP1A_visibilities.append(document['summary']['SP1A']['visibilities'])
+        SP1A_flux.append(document['summary']['SP1A']['flux'])
+        SP1A_clean.append(document['summary']['SP1A']['clean_components'])
+        SP1A_rms.append(document['summary']['SP1A']['rms'])
+
+        SP1B_visibilities.append(document['summary']['SP1B']['visibilities'])
+        SP1B_flux.append(document['summary']['SP1B']['flux'])
+        SP1B_clean.append(document['summary']['SP1B']['clean_components'])
+        SP1B_rms.append(document['summary']['SP1B']['rms'])
+
+        SP2_visibilities.append(document['summary']['SP2']['visibilities'])
+        SP2_flux.append(document['summary']['SP2']['flux'])
+        SP2_clean.append(document['summary']['SP2']['clean_components'])
+        SP2_rms.append(document['summary']['SP2']['rms'])
+
+        SP2A_visibilities.append(document['summary']['SP2A']['visibilities'])
+        SP2A_flux.append(document['summary']['SP2A']['flux'])
+        SP2A_clean.append(document['summary']['SP2A']['clean_components'])
+        SP2A_rms.append(document['summary']['SP2A']['rms'])
+
+        SP2B_visibilities.append(document['summary']['SP2B']['visibilities'])
+        SP2B_flux.append(document['summary']['SP2B']['flux'])
+        SP2B_clean.append(document['summary']['SP2B']['clean_components'])
+        SP2B_rms.append(document['summary']['SP2B']['rms'])
+
+    print("stored")
+    print(MC1_visibilities)
+    df = pd.DataFrame({'Source':sources , 'MC1_visibilities' : MC1_visibilities , 'MC1_flux' : MC1_flux, \
+        'MC1_clean' : MC1_clean, 'MC1_rms' : MC1_rms, 'SC2_visibilities' : SC2_visibilities, \
+        'SC2_flux' : SC2_flux, 'SC2_clean' : SC2_clean, 'SC2_rms' : SC2_rms , 'SC2_visibilities' : SC2_visibilities, \
+        'SC2_flux' : SC2_flux, 'SC2_clean' : SC2_clean, 'SC2_rms' : SC2_rms , 'SC3_visibilities' : SC3_visibilities,\
+        'SC3_flux' : SC3_flux, 'SC3_clean' : SC3_clean, 'SC3_rms' : SC3_rms  , 'SP1_visibilities' : SP1_visibilities,\
+        'SP1_flux' : SP1_flux, 'SP1_clean' : SP1_clean, 'SP1_rms' : SP1_rms , 'SP1A_visibilities' : SP1A_visibilities,\
+        'SP1A_flux' : SP1A_flux, 'SP1A_clean' : SP1A_clean, 'SP1A_rms' : SP1A_rms , 'SP1B_visibilities' : SP1B_visibilities,\
+        'SP1B_flux' : SP1B_flux, 'SP1B_clean' : SP1B_clean, 'SP1B_rms' : SP1A_rms , 'SP2_visibilities' : SP2_visibilities,\
+        'SP1_flux' : SP2_flux, 'SP1_clean' : SP2_clean, 'SP1_rms' : SP2_rms , 'SP2A_visibilities' : SP2A_visibilities,\
+        'SP1A_flux' : SP2A_flux, 'SP1A_clean' : SP2A_clean, 'SP1A_rms' : SP2A_rms , 'SP2B_visibilities' : SP2B_visibilities,\
+        'SP1B_flux' : SP2B_flux, 'SP1B_clean' : SP2B_clean, 'SP1B_rms' : SP2A_rms})
+
+    print(df)
+    
+    
+
+
 
 def scatter_plotting(*args):
     xval = []
@@ -22,37 +176,38 @@ def scatter_plotting(*args):
     cursor = collection.find({'frequency' : frequency})
     for doc in cursor:
         if len(args)==4:
-            stage = args[1]
-            data_val1 = args[2]
+            stage = args[2]
+            data_val2 = args[2]
             data_val2 = args[3]
-            xval.append(doc['summary'][stage][data_val1])
+            xval.append(doc['summary'][stage][data_val2])
             yval.append(doc['summary'][stage][data_val2])
         else:
-            data_val1 = args[1]
             data_val2 = args[2]
-            xval.append(doc['summary']['SP2B'][data_val1] / doc['summary']['MC1'][data_val1])
-            yval.append(doc['summary']['SP2B'][data_val2] / doc['summary']['MC1'][data_val2])
+            data_val2 = args[2]
+            xval.append(doc['summary']['SP2B'][data_val2] / doc['summary']['MC2'][data_val2])
+            yval.append(doc['summary']['SP2B'][data_val2] / doc['summary']['MC2'][data_val2])
 
     fig = plt.figure()
     if len(args)==4:
-        Title = data_val1 + ' Vs. ' + data_val2 + ' (stage ' + stage + ')' 
+        Title = data_val2 + ' Vs. ' + data_val2 + ' (stage ' + stage + ')' 
     else:
-        Title = data_val1 + ' Vs. ' + data_val2
+        Title = data_val2 + ' Vs. ' + data_val2
     fig.suptitle(Title)
-    plt.xlabel(data_val1)
+    plt.xlabel(data_val2)
     plt.ylabel(data_val2)
     plt.scatter(xval, yval)
     plt.show()
 
 def main():
     """if len(sys.argv) < 5:
-        print("Usage: python3 summary_analysis.py <Frequency> <Stage> <Value1> <Value2>")
-        exit(1)"""
-    if len(sys.argv) == 4:
-        scatter_plotting(sys.argv[1], sys.argv[2], sys.argv[3])
-    elif len(sys.argv) == 5:
-        scatter_plotting(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+        print("Usage: python3 summary_analysis.py <Frequency> <Stage> <Value2> <Value2>")
+        exit(2)"""
+    # if len(sys.argv) == 4:
+    #     scatter_plotting(sys.argv[2], sys.argv[2], sys.argv[3])
+    # elif len(sys.argv) == 5:
+    #     scatter_plotting(sys.argv[2], sys.argv[2], sys.argv[3], sys.argv[4])
+
+    read_database('610')
 
 if __name__ == "__main__":
     main()
-
