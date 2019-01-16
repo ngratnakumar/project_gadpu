c47154387c80787448b6d14638274cbbc1c34120
Sayali Deshpande <sayali.d.deshp@gmail.com>
2019-01-16 16:19:09 +0530

    Update plot_kde and remove hard-coded part in create_dataframe_from_db

--- summary_analysis.py
+++ summary_analysis.py
@@ -7,17 +7,15 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import axes3d, Axes3D
-from scipy import stats
-from matplotlib.cbook import boxplot_stats
 from itertools import combinations
 
-dbname = "summary_db"
+DB_NAME = "summary_db"
 
 # Connect to mongodb as a client
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient[dbname]
+MY_CLIENT = pymongo.MongoClient("mongodb://localhost:27017/")
+MY_DB = MY_CLIENT[DB_NAME]
 
-pp = pprint.PrettyPrinter()
+#pp = pprint.PrettyPrinter()
 
 STAGES = ['MC1',
           'SC1',
@@ -34,121 +32,58 @@ STAGES = ['MC1',
 
 ATTRIBUTES = ['visibilities',
               'flux',
-              'clean',
+              'clean_components',
               'rms',
             ]
 
 FREQUENCIES = [325, 610]
-
+CMAPS = ['Blues', 'Purples', 'Oranges', 'Greens', 'Reds']
 CLIP_LIMITS = [325, 'MC1', 'visibilities', 50000, 325, 'MC1', 'flux', 6]
-
-
 
 def create_dataframe_from_db():
     print("Creating dataframe...")
     df = pd.DataFrame()
-    collections = mydb.list_collection_names()
+    collections = MY_DB.list_collection_names()
     for col_name in collections:
-        collection = mydb[col_name]
+        collection = MY_DB[col_name]
         cursor = collection.find({})
         for document in cursor:
-            flag = 1
-            frequency = document["frequency"]
-            cycle = int(col_name[5:])
-
-            MC1_visibilities = document['summary']['MC1']['visibilities']
-            MC1_flux = document['summary']['MC1']['flux']
-            MC1_clean = document['summary']['MC1']['clean_components']
-            MC1_rms = document['summary']['MC1']['rms']
-
-            SC1_visibilities = document['summary']['SC1']['visibilities']
-            SC1_flux = document['summary']['SC1']['flux']
-            SC1_clean = document['summary']['SC1']['clean_components']
-            SC1_rms = document['summary']['SC1']['rms']
-
-            SC2_visibilities = document['summary']['SC2']['visibilities']
-            SC2_flux = document['summary']['SC2']['flux']
-            SC2_clean = document['summary']['SC2']['clean_components']
-            SC2_rms = document['summary']['SC2']['rms']
-
-            SC3_visibilities = document['summary']['SC3']['visibilities']
-            SC3_flux = document['summary']['SC3']['flux']
-            SC3_clean = document['summary']['SC3']['clean_components']
-            SC3_rms = document['summary']['SC3']['rms']
-
-            try:
-                SP1_visibilities = document['summary']['SP1']['visibilities']
-                SP1_flux = document['summary']['SP1']['flux']
-                SP1_clean = document['summary']['SP1']['clean_components']
-                SP1_rms = document['summary']['SP1']['rms']
-
-            except KeyError:
-                flag = 0
-                SP1_visibilities = document['summary']['SP0']['visibilities']
-                SP1_flux = document['summary']['SP0']['flux']
-                SP1_clean = document['summary']['SP0']['clean_components']
-                SP1_rms = document['summary']['SP0']['rms']
-
-            SP1A_visibilities = document['summary']['SP1A']['visibilities']
-            SP1A_flux = document['summary']['SP1A']['flux']
-            SP1A_clean = document['summary']['SP1A']['clean_components']
-            SP1A_rms = document['summary']['SP1A']['rms']
-
-            SP1B_visibilities = document['summary']['SP1B']['visibilities']
-            SP1B_flux = document['summary']['SP1B']['flux']
-            SP1B_clean = document['summary']['SP1B']['clean_components']
-            SP1B_rms = document['summary']['SP1B']['rms']
-
-            SP2_visibilities = document['summary']['SP2']['visibilities']
-            SP2_flux = document['summary']['SP2']['flux']
-            SP2_clean = document['summary']['SP2']['clean_components']
-            SP2_rms = document['summary']['SP2']['rms']
-
-            SP2A_visibilities = document['summary']['SP2A']['visibilities']
-            SP2A_flux = document['summary']['SP2A']['flux']
-            SP2A_clean = document['summary']['SP2A']['clean_components']
-            SP2A_rms = document['summary']['SP2A']['rms']
-
-            SP2B_visibilities = document['summary']['SP2B']['visibilities']
-            SP2B_flux = document['summary']['SP2B']['flux']
-            SP2B_clean = document['summary']['SP2B']['clean_components']
-            SP2B_rms = document['summary']['SP2B']['rms']
-
-            df = df.append({'Cycle' : cycle, 'Frequency' : frequency, 'MC1_visibilities' : MC1_visibilities , 'MC1_flux' : MC1_flux, \
-                'MC1_clean' : MC1_clean, 'MC1_rms' : MC1_rms, 'SC1_visibilities' : SC1_visibilities, \
-                'SC1_flux' : SC1_flux, 'SC1_clean' : SC1_clean, 'SC1_rms' : SC1_rms, 'SC2_visibilities' : SC2_visibilities, \
-                'SC2_flux' : SC2_flux, 'SC2_clean' : SC2_clean, 'SC2_rms' : SC2_rms, 'SC3_visibilities' : SC3_visibilities,\
-                'SC3_flux' : SC3_flux, 'SC3_clean' : SC3_clean, 'SC3_rms' : SC3_rms, 'SP1_visibilities' : SP1_visibilities,\
-                'SP1_flux' : SP1_flux, 'SP1_clean' : SP1_clean, 'SP1_rms' : SP1_rms, 'SP1A_visibilities' : SP1A_visibilities,\
-                'SP1A_flux' : SP1A_flux, 'SP1A_clean' : SP1A_clean, 'SP1A_rms' : SP1A_rms, 'SP1B_visibilities' : SP1B_visibilities,\
-                'SP1B_flux' : SP1B_flux, 'SP1B_clean' : SP1B_clean, 'SP1B_rms' : SP1B_rms, 'SP2_visibilities' : SP2_visibilities,\
-                'SP2_flux' : SP2_flux, 'SP2_clean' : SP2_clean, 'SP2_rms' : SP2_rms, 'SP2A_visibilities' : SP2A_visibilities,\
-                'SP2A_flux' : SP2A_flux, 'SP2A_clean' : SP2A_clean, 'SP2A_rms' : SP2A_rms, 'SP2B_visibilities' : SP2B_visibilities,\
-                'SP2B_flux' : SP2B_flux, 'SP2B_clean' : SP2B_clean, 'SP2B_rms' : SP2B_rms, 'SP1_flag' : flag}, ignore_index=True)
-
-        df.head()
-        df.to_pickle("summary.pkl")
-        print("Pickle file created.")
+            dict = {}
+            dict['Frequency'] = document["frequency"]
+            dict['Cycle'] = int(col_name[5:])
+            dict['SP1_flag'] = 1
+            for stage in STAGES:
+                for attribute in ATTRIBUTES:
+                    column_name = stage +'_' + attribute
+                    try :
+                        dict[ column_name ] = document['summary'][stage][attribute]
+                    except KeyError:
+                        dict['SP1_flag'] = 0
+                        dict[column_name] = document['summary']['SP0'][attribute]
+            df = df.append(dict, ignore_index=True)
+    df.head()
+    df.to_pickle("summary.pkl")
+    print("Pickle file created.")
 
 def get_data_frame():
     df = pd.read_pickle('summary.pkl')
     return df
 
-def clip_df(df, args_list):
-    if len(args_list) % 4 != 0:
-        print("Wrong format for clip_df. Should be <frequency> <stage> <attribute> <limit>")
+def clip_df( df, args_list ):
+    if len( args_list )%4 != 0:
+        print("Wrong format of args_list in clip_df. Should be [<frequency>, <stage>, <attribute>, <limit>, ...]")
         exit(1)
     num_limits = len(args_list) // 4
     print("Initial df shape:", df.shape)
     for i in range(num_limits):
-        column_name = args_list[i*4 + 1] + '_' + args_list[i*4 + 2]
-        limit = int(args_list[i*4 + 3])
-        frequency = str(args_list[i*4])
-        df = df.loc[((df['Frequency'] == frequency) & (df[column_name] < limit) ) | (df['Frequency'] != frequency)]
+        column_name = args_list[ i*4 + 1 ] + '_' + args_list[ i*4 + 2 ]
+        limit = int( args_list[ i*4 + 3 ] )
+        frequency = float( args_list[ i*4 ] )
+        df = df.loc[ ((df['Frequency'] == frequency) & (df[ column_name ] < limit)) | (df['Frequency'] != frequency) ]
     print("Final df shape:", df.shape)
     return df
 
-def select_plot(df, plot_name):
+def select_plot( df, plot_name ):
     plot_name = plot_name.lower()
     if plot_name == 'kde':
         plot_kde(df)
@@ -164,11 +99,11 @@ def select_plot(df, plot_name):
         plot_strip(df)
 
 def plot_kde(df):
-    for frequency in FREQUENCIES:
-        df_temp = df.loc[df['Frequency'] == str(frequency)]
+    for c, frequency in enumerate(FREQUENCIES):
+        df_temp = df.loc[ (df['Frequency'] == frequency) & (df['SP1_flag'] != 0) ]
         for stage in STAGES:
             combs = list( combinations(ATTRIBUTES, 2) )
-            plt.suptitle("KDE plot for frequency: " + str(frequency) + " stage: " + stage)
+            plt.suptitle("KDE plot for Frequency: " + str(frequency) + ", Stage: " + stage)
             for i, comb in list(enumerate(combs, 1)):
                 xlabel, ylabel = comb
                 data_x = df_temp[stage + '_' + xlabel]
@@ -176,10 +111,11 @@ def plot_kde(df):
                 plt.subplots_adjust( hspace=0.5, wspace=0.5 )
                 plt.subplot(2, 3, i)
                 plt.title(ylabel + ' v/s ' + xlabel)
-                sns.kdeplot(data_x, data_y, cmap='Blues', shade=True, shade_lowest=False)
-                plt.scatter(data_x, data_y, s=1 )
-            manager = plt.get_current_fig_manager()
-            manager.resize(*manager.window.maxsize())
+                sns.kdeplot(data_x, data_y, cmap=CMAPS[c], cbar=True, shade=True, shade_lowest=False)
+                plt.scatter(data_x, data_y, s=1, c=CMAPS[c][:-1], cmap=CMAPS[c] )
+            if matplotlib.get_backend() == 'TkAgg':
+                manager = plt.get_current_fig_manager()
+                manager.resize(*manager.window.maxsize())
             plt.show()
 
 def plot_histogram(df):
@@ -200,12 +136,6 @@ def plot_histogram(df):
                 plt.ylabel('Frequency')
                 plt.title('Histogram: Stage='+stage+' Field:'+i)
                 n, bins, patches=plt.hist(x=df_temp[str3], bins=20, color='#0504aa',alpha=0.7, rwidth=0.85)
-
-                #maxfreq=n.max()
-
-            manager=plt.get_current_fig_manager()
-            #manager.resize(*manager.window.maxsize())
-            #plt.savefig('Hist/Histo_'+str(frequency)+'_'+stage+'_'+i)
             plt.show()
 
 def plot_heat_map(df):
@@ -255,8 +185,9 @@ def plot_heat_map(df):
         sns.heatmap(corr2, mask=mask, cmap=cmap, vmax=.3, center=0,\
             square=True, linewidths=.5, cbar_kws={"shrink": .5}, annot=True, fmt=".2f")
 
-        manager = plt.get_current_fig_manager()
-        manager.resize(*manager.window.maxsize())
+        if matplotlib.get_backend() == 'TkAgg':
+            manager = plt.get_current_fig_manager()
+            manager.resize(*manager.window.maxsize())
         plt.show()
 
 def plot_3d_scatter(df):
@@ -301,7 +232,7 @@ def plot_strip(df):
     df = df.drop("SP1_flag", 1)
 
     for attribute in ATTRIBUTES:
-        new_df = pd.DataF-rame()
+        new_df = pd.DataFrame()
         for stage in STAGES:
             query_string = stage + "_" + attribute
             new_df[query_string] = df[query_string]
@@ -336,9 +267,10 @@ def splot(df):
         handletextpad=0, columnspacing=1,\
         loc="lower right", ncol=3, frameon=True)
 
+    if matplotlib.get_backend() == 'TkAgg':
+        manager = plt.get_current_fig_manager()
+        manager.resize(*manager.window.maxsize())
     plt.show()
-
-
 
 def plot_scatter(df):
     df = df[df["SP1_flag"] == 1]
@@ -359,6 +291,9 @@ def plot_scatter(df):
             manager.resize(*manager.window.maxsize())
             plt.show()
 
+def print_stats(df):
+    for frequency in FREQUENCIES:
+        print( "Number of data points for frequency", frequency, ":", df.loc[df['Frequency'] == frequency].shape[0] )
 
 def main():
     if len(sys.argv) < 2:
@@ -375,7 +310,8 @@ def main():
         create_dataframe_from_db()
         df = get_data_frame()
 
-    df = clip_df(df, CLIP_LIMITS)
+    #df = clip_df(df, CLIP_LIMITS)
+    print_stats(df)
     plot_name = sys.argv[1]
     select_plot(df, plot_name)
 
